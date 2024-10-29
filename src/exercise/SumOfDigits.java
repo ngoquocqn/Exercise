3fa52ef18a13da952e516df96d63efe30782644d
Ngô Tấn Quốc <Ngotanquocqn@gmail.com>
2024-10-29 17:05:32 +0700

    Initial

---
+++ src/exercise/SumOfDigits.java
@@ -0,0 +1,13 @@
+package exercise;
+
+public class SumOfDigits {
+
+    public static int sumDigits(int number) {
+        int sum = 0;
+        while (number != 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+        return sum;
+    }
+}
